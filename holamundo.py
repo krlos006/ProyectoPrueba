f79019806d69dc92a16e201283bb51e1cd85374d
Carlos Ramos <krlos006@gmail.com>
2026-07-20 16:29:56 -0400

    Agregue funcion calculo de edad

--- holamundo.py
+++ holamundo.py
@@ -1,2 +1,28 @@
+#Hola Mundo en Python
 print("¡Hola, desde Visual Studio Code!")
 print("¡Estamos avanzando!")
+
+#Calculo de edad
+from datetime import datetime
+hoy = datetime.now()
+meses = (
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+)
+numero_mes = hoy.month
+nombre_mes = meses[numero_mes - 1]
+print("hoy es:", hoy.day, "de", nombre_mes, "de", hoy.year)
+nombre = input("Ingrese su nombre: ")
+año_nacimiento = int(input("Ingrese su año de nacimiento: "))
+mes_nacimiento = int(input("Ingrese su mes de nacimiento (1-12): "))
+dia_nacimiento = int(input("Ingrese su día de nacimiento (1-31): "))
+año_actual = hoy.year
+mes_actual = hoy.month
+dia_actual = hoy.day
+#Calculo años desde nacimiento hasta la fecha actual
+edad = año_actual - año_nacimiento
+#Valido si ya paso su mes/dia de nacimiento para restar un año si no ha pasado
+if mes_actual < mes_nacimiento or (mes_actual == mes_nacimiento and dia_actual < dia_nacimiento):
+    edad -= 1
+#Muestro el resultado
+print(f"La edad de {nombre} es: {edad} años")
